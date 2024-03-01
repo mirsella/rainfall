3635e1545d4597b2bd616816c1b9a1644a05f430
mirsella <mirsella@protonmail.com>
2024-03-01 09:55:57 +0100

    add function m

--- level7/source.c
+++ level7/source.c
@@ -1,4 +1,14 @@
-undefined4 main(undefined4 param_1,int param_2)
+void m(void *param_1, int param_2, char *param_3, int param_4, int param_5)
+
+{
+  time_t tVar1;
+
+  tVar1 = time((time_t *)0x0);
+  printf("%s - %d\n", c, tVar1);
+  return;
+}
+
+undefined4 main(undefined4 param_1, int param_2)
 
 {
   undefined4 *puVar1;
@@ -14,10 +24,10 @@ undefined4 main(undefined4 param_1,int param_2)
   *puVar3 = 2;
   pvVar2 = malloc(8);
   puVar3[1] = pvVar2;
-  strcpy((char *)puVar1[1],*(char **)(param_2 + 4));
-  strcpy((char *)puVar3[1],*(char **)(param_2 + 8));
-  __stream = fopen("/home/user/level8/.pass","r");
-  fgets(c,0x44,__stream);
+  strcpy((char *)puVar1[1], *(char **)(param_2 + 4));
+  strcpy((char *)puVar3[1], *(char **)(param_2 + 8));
+  __stream = fopen("/home/user/level8/.pass", "r");
+  fgets(c, 0x44, __stream);
   puts("~~");
   return 0;
 }
